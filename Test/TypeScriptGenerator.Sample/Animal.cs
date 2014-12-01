734e992619519c0322e47c4a3c2bf0e0b40074ec
paul.welter <pwelter@loresoft.com>
2014-11-30 23:16:00 -0600

    first version

---
+++ Test/TypeScriptGenerator.Sample/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeScriptGenerator.Sample
+{
+    public abstract class Pet
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+
+    public class Dog : Pet
+    {
+        public string Bread { get; set; }
+    }
+
+    public class Cat : Pet
+    {
+        public string Family { get; set; }
+    }
+}
---
+++ Test/TypeScriptGenerator.Test/InterfaceTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Xunit;
+
+namespace TypeScriptGenerator.Test
+{
+    public class InterfaceTest
+    {
+        [Fact]
+        public void LoadType()
+        {
+            string assemblyFile = @"..\..\..\TypeScriptGenerator.Sample\bin\Debug\TypeScriptGenerator.Sample.dll";
+            string rootClass = "TypeScriptGenerator.Sample.Task";
+
+
+            var scriptInterface = Generator.CreateInterface(assemblyFile, rootClass);
+            foreach (var property in scriptInterface.Properties)
+            {
+                Console.WriteLine(property);
+            }
+        }
+
+        [Fact]
+        public void IsNullable()
+        {
+            string assemblyFile = @"..\..\..\TypeScriptGenerator.Sample\bin\Debug\TypeScriptGenerator.Sample.dll";
+            string rootClass = "TypeScriptGenerator.Sample.Task";
+
+            var moduleDefinition = ModuleDefinition.ReadModule(assemblyFile);
+            var typeDefintion = moduleDefinition.GetType(rootClass);
+
+            foreach (var propertyDefinition in typeDefintion.Properties)
+            {
+                Console.WriteLine("Name: " + propertyDefinition.Name);
+                Console.WriteLine("    " + propertyDefinition.PropertyType.FullName);
+                var b = propertyDefinition.PropertyType.GetUnderlyingType();
+                Console.WriteLine("    " + b.FullName);
+            }
+        }
+
+        [Fact]
+        public void LoadTypeInheritance()
+        {
+            string assemblyFile = @"..\..\..\TypeScriptGenerator.Sample\bin\Debug\TypeScriptGenerator.Sample.dll";
+            string rootClass = "TypeScriptGenerator.Sample.Dog";
+
+
+            var scriptInterface = Generator.CreateInterface(assemblyFile, rootClass);
+            foreach (var property in scriptInterface.Properties)
+            {
+                Console.WriteLine(property);
+            }
+        }
+    }
+}
